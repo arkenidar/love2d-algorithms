c4d12820fc11e17494a2450b627bcb1ad3984c9c
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-01-09 19:49:03 +0100

    point_angle_from_center(point_mouse_position(), { center_x, center_y })

--- main.lua
+++ main.lua
@@ -12,21 +12,21 @@ end
 
 -- rettangolo sponde
 local function rectangle_bounds(rectangle)
-  return {rectangle[1], rectangle[2], rectangle[1]+rectangle[3], rectangle[2]+rectangle[4] }
+  return { rectangle[1], rectangle[2], rectangle[1] + rectangle[3], rectangle[2] + rectangle[4] }
 end
 
 -- rettangolo da sponde
 local function rectangle_from_bounds(bounds)
-  return {bounds[1], bounds[2], bounds[3]-bounds[1], bounds[4]-bounds[2] }
+  return { bounds[1], bounds[2], bounds[3] - bounds[1], bounds[4] - bounds[2] }
 end
 
 local function rectangle_bounds_intersect_boolean(bounds1, bounds2)
-  local intersect_boolean = 
-  bounds1[1] < bounds2[3] and
-  bounds1[3] > bounds2[1] and
-  bounds1[2] < bounds2[4] and
-  bounds1[4] > bounds2[2]
-  return intersect_boolean  
+  local intersect_boolean =
+      bounds1[1] < bounds2[3] and
+      bounds1[3] > bounds2[1] and
+      bounds1[2] < bounds2[4] and
+      bounds1[4] > bounds2[2]
+  return intersect_boolean
 end
 
 -- rettangolo operazione intersezione
@@ -34,19 +34,19 @@ end
 local function rectangle_operation_intersection(rectangle1, rectangle2)
   local bounds1 = rectangle_bounds(rectangle1)
   local bounds2 = rectangle_bounds(rectangle2)
-  
+
   local intersect_boolean = rectangle_bounds_intersect_boolean(bounds1, bounds2)
-  
+
   if not intersect_boolean then
     return nil
   end
-  
+
   local intersection_rectangle
   local bounds3 = {
     math.max(bounds1[1], bounds2[1]),
     math.max(bounds1[2], bounds2[2]),
     math.min(bounds1[3], bounds2[3]),
-    math.min(bounds1[4], bounds2[4])  
+    math.min(bounds1[4], bounds2[4])
   }
   intersection_rectangle = rectangle_from_bounds(bounds3)
   return intersection_rectangle
@@ -55,14 +55,14 @@ end
 ------------------------------------------------
 local function point_inside_rectangle(point, rectangle)
   return
-    point[1]>=rectangle[1] and
-    point[1]<=(rectangle[1]+rectangle[3]) and
-    point[2]>=rectangle[2] and
-    point[2]<=(rectangle[2]+rectangle[4])
+      point[1] >= rectangle[1] and
+      point[1] <= (rectangle[1] + rectangle[3]) and
+      point[2] >= rectangle[2] and
+      point[2] <= (rectangle[2] + rectangle[4])
 end
 
 local function point_mouse_position()
-  return {love.mouse.getX(), love.mouse.getY()}
+  return { love.mouse.getX(), love.mouse.getY() }
 end
 ------------------------------------------------
 local function input_touching()
@@ -74,40 +74,42 @@ end
 function love.draw()
   -- *****************************************************
 
-  local rectangle1 = {50, 30, 50, 100}
-  local color1 = {1,0,1}
+  local rectangle1 = { 50, 30, 50, 100 }
+  local color1 = { 1, 0, 1 }
   draw_rectangle_color(rectangle1, color1)
-  
-  local rectangle2 = {love.mouse.getX(), love.mouse.getY(), rectangle1[3], rectangle1[4] }
-  local color2 = {0,1,1}
+
+  local rectangle2 = { love.mouse.getX(), love.mouse.getY(), rectangle1[3], rectangle1[4] }
+  local color2 = { 0, 1, 1 }
   draw_rectangle_color(rectangle2, color2)
-  
+
   -- - intersezione di 2 rettagoli -> rettangolo anche nullo : rectangle_operation_intersection
   local rectangle3 = rectangle_operation_intersection(rectangle1, rectangle2)
-  local color3 = {1,0,0}
+  local color3 = { 1, 0, 0 }
   if rectangle3 then draw_rectangle_color(rectangle3, color3) end
-  
+
   -- *****************************************************
-  
+
   -- - 1 punto interno ad un 1 rettangolo -> vero/falso : point_inside_rectangle
-  
-  local rectangle_mouse_inside = { color = {0.7,1,0.2} , 150,30, 50,100 }
+
+  local rectangle_mouse_inside = { color = { 0.7, 1, 0.2 }, 150, 30, 50, 100 }
   if point_inside_rectangle(point_mouse_position(), rectangle_mouse_inside) then
-    rectangle_mouse_inside.color = {0.5, 0.5, 0.7} end
-  draw_rectangle_color(rectangle_mouse_inside, rectangle_mouse_inside.color )
-  
+    rectangle_mouse_inside.color = { 0.5, 0.5, 0.7 }
+  end
+  draw_rectangle_color(rectangle_mouse_inside, rectangle_mouse_inside.color)
+
   -- *****************************************************
-  
+
   -- - 1 punto interno ad un 1 rettangolo -> vero/falso : point_inside_rectangle
-  
-  local rectangle_mouse_touch = { color = {0.7,1,0.2} , 250,30, 50,100 }
+
+  local rectangle_mouse_touch = { color = { 0.7, 1, 0.2 }, 250, 30, 50, 100 }
   if input_touching() and
-  point_inside_rectangle(point_mouse_position(), rectangle_mouse_touch) then
-    rectangle_mouse_touch.color = {0.5, 0.5, 0.7} end
-  draw_rectangle_color(rectangle_mouse_touch, rectangle_mouse_touch.color )
-  
+      point_inside_rectangle(point_mouse_position(), rectangle_mouse_touch) then
+    rectangle_mouse_touch.color = { 0.5, 0.5, 0.7 }
+  end
+  draw_rectangle_color(rectangle_mouse_touch, rectangle_mouse_touch.color)
+
   -- *****************************************************
-  
+
   -- - 1 punto interno ad un 1 poligono convesso -> vero/falso : point_inside_polygon_convex
 
   --[[ PIP algorithms for convex and concave polygons
@@ -115,16 +117,25 @@ function love.draw()
     ]]
 
   --[[ external links for inspiration:
-  
+
   - original in DHTML (HTML5+ES)
   <https://github.com/arkenidar/point_in_polygon>
 
   - derivation for Lua and Love2D
   <https://github.com/arkenidar/Lua_Love2D_PointInPolygon-Algorithm>
-  
+
   ]]
 
   -- ...
 
   -- *****************************************************
+
+  require("utils_math")
+
+  local center_x, center_y = 150, 150
+  local angle, angle1, angle2 = point_angle_from_center(point_mouse_position(), { center_x, center_y })
+  love.graphics.print("angle: " .. tostring(math.floor(angle)) .. "°", center_x, center_y)
+
+  --love.graphics.print("angle1: ".. tostring( math.floor(angle1) ).."°", center_x,center_y+15*1)
+  --love.graphics.print("angle2: ".. tostring( math.floor(angle2) ).."°", center_x,center_y+15*2)
 end
