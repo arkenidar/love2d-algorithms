7c4d00d52379a9475cd643a7cbc3ae4f2bedb4f3
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-01-11 15:24:37 +0100

    [capability] 2 ring modes (selectable by click)

--- main.lua
+++ main.lua
@@ -69,21 +69,42 @@ local function input_touching()
   return love.mouse.isDown(1)
 end
 ------------------------------------------------
+function love.load()
+  love.window.setTitle("arkenidar / love2d-algorithms")
+end
+
+------------------------------------------------
+local input_touched = false
+local input_touching_previously = false
+
+local mode_rings = 0 -- 2 ring modes (selectable)
 
 -- disegna
 function love.draw()
+  -- input_touched
+  local input_touching_currently = input_touching()
+  input_touched = input_touching_currently and not input_touching_previously
+  input_touching_previously = input_touching_currently
+
   -- *****************************************************
 
   local rectangle1 = { 50, 30, 50, 100 }
-  local color1 = { 1, 0, 1 }
+  local color1 = { 0, 0, 1 }
   draw_rectangle_color(rectangle1, color1)
 
   local rectangle2 = { love.mouse.getX(), love.mouse.getY(), rectangle1[3], rectangle1[4] }
-  local color2 = { 0, 1, 1 }
-  draw_rectangle_color(rectangle2, color2)
+  -- post-poned
 
   -- - intersezione di 2 rettagoli -> rettangolo anche nullo : rectangle_operation_intersection
   local rectangle3 = rectangle_operation_intersection(rectangle1, rectangle2)
+
+  if rectangle3 then
+    -- post-poned to here (after intersection check)
+    local color2 = { 0, 1, 1 }
+    draw_rectangle_color(rectangle2, color2)
+  end
+
+  -- rectangle 3 after rectangle2
   local color3 = { 1, 0, 0 }
   if rectangle3 then draw_rectangle_color(rectangle3, color3) end
 
@@ -93,7 +114,7 @@ function love.draw()
 
   local rectangle_mouse_inside = { color = { 0.7, 1, 0.2 }, 150, 30, 50, 100 }
   if point_inside_rectangle(point_mouse_position(), rectangle_mouse_inside) then
-    rectangle_mouse_inside.color = { 0.5, 0.5, 0.7 }
+    rectangle_mouse_inside.color = { 1, 0.5, 0.7 }
   end
   draw_rectangle_color(rectangle_mouse_inside, rectangle_mouse_inside.color)
 
@@ -102,10 +123,20 @@ function love.draw()
   -- - 1 punto interno ad un 1 rettangolo -> vero/falso : point_inside_rectangle
 
   local rectangle_mouse_touch = { color = { 0.7, 1, 0.2 }, 250, 30, 50, 100 }
-  if input_touching() and
-      point_inside_rectangle(point_mouse_position(), rectangle_mouse_touch) then
-    rectangle_mouse_touch.color = { 0.5, 0.5, 0.7 }
+
+  local point_is_inside = point_inside_rectangle(point_mouse_position(), rectangle_mouse_touch)
+
+  -- if currently touching then color it
+  if input_touching() and point_is_inside then
+    rectangle_mouse_touch.color = { 1, 0.5, 0.7 }
   end
+
+  -- if touched (one time) change "mode" variable
+  if input_touched and point_is_inside then
+    -- 2 ring modes (selectable)
+    mode_rings = 1 - mode_rings -- 0 to 1. 1 to 0.
+  end
+
   draw_rectangle_color(rectangle_mouse_touch, rectangle_mouse_touch.color)
 
   -- *****************************************************
@@ -130,32 +161,72 @@ function love.draw()
 
   -- *****************************************************
 
+  -- reusable functions (module)
   require("utils_math")
 
+  -- circle data
   local center_x, center_y = 150, 250
   local radius = 50
 
-  local angle, angle1, angle2 = point_angle_from_center(point_mouse_position(), { center_x, center_y })
-  love.graphics.print("angle: " .. tostring(math.floor(angle)) .. "°", center_x, center_y - radius - 50)
+  -- move the "zero-angle", rotation
+  local angle_offset = 90 -- degrees
 
-  --love.graphics.print("angle1: ".. tostring( math.floor(angle1) ).."°", center_x,center_y+15*1)
-  --love.graphics.print("angle2: ".. tostring( math.floor(angle2) ).."°", center_x,center_y+15*2)
+  -- angle from mouse
+  local angle = point_angle_from_center(point_mouse_position(), { center_x, center_y })
+  angle = (angle + angle_offset) % 360
+  angle = math.floor(angle)
 
-  -- circle, ring, sector
+  -- texts
+  love.graphics.setColor(0, 1, 0)
+  love.graphics.print("angle: " .. tostring(angle) .. "°", center_x, center_y - radius - 50)
+  love.graphics.print("ring mode: " .. tostring(mode_rings), center_x, center_y - radius - 50 - 20)
+
+  -- circle derivations: ring and sector conditions
+  love.graphics.setColor(0, 1, 0)
+
+  -- for each pixel in rectangle
   for px = center_x - radius, center_x + radius do
     for py = center_y - radius, center_y + radius do
-      local condition = true
-
       -- ring conditions
+      local condition_ring
       local distance = point_distance_from_point({ center_x, center_y }, { px, py })
-      condition = condition and distance <= radius and distance >= radius / 1.5
+      condition_ring = distance <= radius and distance >= (radius / 1.3)
 
       -- sector conditions
-      local angle_point = point_angle_from_center({ px, py }, { center_x, center_y })
-      condition = condition and angle_point <= angle
 
-      if condition then
-        love.graphics.points({ px, py })
+      -- angle_point is angle related to current pixel position
+      local angle_point = point_angle_from_center({ px, py }, { center_x, center_y })
+      angle_point = (angle_point + angle_offset) % 360
+
+      -- range of angles
+      local angle_begin, angle_end
+
+      -- 2 ring modes (selectable)
+      if mode_rings == 0 then
+        angle_begin = 0; angle_end = angle
+      elseif mode_rings == 1 then
+        local fixed_sector = 60
+        angle_begin = ((angle - fixed_sector / 2) + 360) % 360
+        angle_end = (angle + fixed_sector / 2) % 360
+      end
+
+      --love.graphics.print("angle_begin: " .. tostring(angle_begin) .. "°", center_x + radius + 20, center_y)
+      --love.graphics.print("angle_end: " .. tostring(angle_end) .. "°", center_x + radius + 20, center_y + 20)
+
+      local condition_sector
+      if angle_begin >= angle_end then
+        -- consider zero-angle discontinuity of angles
+        condition_sector = angle_point >= angle_begin and angle_point <= 359
+        condition_sector = condition_sector or (angle_point >= 0 and angle_point <= angle_end)
+      else
+        -- base case, simpler
+        condition_sector = angle_point >= angle_begin and angle_point <= angle_end
+      end
+
+      -- combined conditions
+      local condition_point = condition_ring and condition_sector
+      if condition_point then
+        love.graphics.points({ px, py }) -- draw pixel
       end
     end
   end
